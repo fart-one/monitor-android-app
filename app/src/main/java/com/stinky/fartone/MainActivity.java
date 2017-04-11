ca800d0b6d106348f9944787c8b9f84616a02620
Calcium <wpawel89@gmail.com>
2017-04-11 17:38:58 +0200

    Added : ->ButterKnife dependency ->INTERNET permission Implemented: ->Simple working MQTT client on main activity [to change]

--- app/src/main/java/com/stinky/fartone/MainActivity.java
+++ app/src/main/java/com/stinky/fartone/MainActivity.java
@@ -5,11 +5,37 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String broker = "tcp://mqtt.gawra.net.pl:1883";
+    final String mqttClientId = "and_app";
+    final String LOG_TAG_MAIN = "LOG_MAIN";
+
+    @BindView(R.id.mqtt_message)
+    TextView mqttMessage;
+    @BindView(R.id.subscribe_button)
+    Button button;
+
+    MqttClient simpleClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        try {
+            Log.i(LOG_TAG_MAIN, "Creating client.");
+            simpleClient = new MqttClient(broker, mqttClientId, new MemoryPersistence());
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setUserName("fart1");
+            connOpts.setPassword("farting-toilet".toCharArray());
+            simpleClient.connect(connOpts);
+            Log.i(LOG_TAG_MAIN, "Connected");
+            Log.i(LOG_TAG_MAIN, "Is really connected? : " + simpleClient.isConnected());
+        } catch (MqttException e) {
+            System.err.println(e);
+        }
     }
 
     @Override
@@ -48,5 +89,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.subscribe_button) void subscribe(View view) {
+        try {
+            Log.i(LOG_TAG_MAIN, "Subscribing");
+            simpleClient.subscribe("test/app/", 1);
+            simpleClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    System.err.println(cause);
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    System.out.println("Got message!");
+                    System.out.println("Topic: " + topic);
+                    System.out.println("Message: ");
+                    System.out.println(message);
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    System.err.println(token);
+                }
+            });
+        } catch (MqttException e) {
+            System.err.println(e);
+        }
     }
 }
